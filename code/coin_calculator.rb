6d3f77c7e129bb86035a3c684732f39eaaf38ef4
Dennis Bartuschat <dennis.bartu@gmail.com>
2018-01-18 21:25:55 +0100

    Added while loop, some code clean-up

--- code/coin_calculator.rb
+++ code/coin_calculator.rb
@@ -1,17 +1,22 @@
 require 'httparty'
 
 print "$$ BTC to EUR Calculator $$ \r\n"
-print "How much BTC do you got? \r\n"
 
-n = gets.to_f
-url = "https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR"
+while(true)
+  print "How much BTC do you got? \r\n"
 
-response = HTTParty.get(url)
-parsedResponse =  response.parsed_response
-eurPrice = parsedResponse[0]["price_eur"]
+  n = gets.to_f
+  url = "https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR"
 
-calculatedPrice = eurPrice.to_f * n
+  response = HTTParty.get(url)
+  eurPrice = response[0]["price_eur"]
+  calculatedPrice = eurPrice.to_f * n
 
-print "Actual selling price is: "
-print calculatedPrice
-print "\r\n"
+  print "Actual selling price is: "
+  print calculatedPrice
+  print "\r\n"
+
+  print "Press 'R' for another calculation \r\n"
+  break if gets.chomp != "R"
+end
+
